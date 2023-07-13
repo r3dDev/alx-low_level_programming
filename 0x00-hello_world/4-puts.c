ff49432fcf0810aed1df2ba6dd999c9f71962ac2
redouane <afoulous.redouane@gmail.com>
2023-07-13 03:59:25 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
-int main(void){
+/*
+ * main - Entry point 
+ * puts a sentence and returns 0
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
